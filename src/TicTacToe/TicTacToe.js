4e63eb481fe810f3e964914c02c5305c8e813318
psyfer <psyferblock.io@gmail.com>
2022-06-14 20:13:20 +0300

    adding variables/props

--- src/TicTacToe/TicTacToe.js
+++ src/TicTacToe/TicTacToe.js
@@ -1,30 +1,41 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './TicTacToe.css';
 
 const TicTacToe = () => {
+    const [turn,setTurn]=useState('x');
+    const[cells,setCells]=useState[Array(9).fill('')]
+    const handleClick=(num)=>{
+        let squares=[...cells]
+        if (turn=='x'){
+            setTurn('o')
+        }else{
+            setTurn('x')
+        }
+    }
     const Cell=()=>{
-        return <td>-</td>
+        return <td onClick={()=>handleClick()}>-</td>
     }
   return (
     <div className='container'>
         <table>
+            Turn:{turn}
             <body>
                 <tr>
-                    <Cell/>
-                    <Cell/>
-                    <Cell/>
+                    <Cell num={0}/>
+                    <Cell num={1}/>
+                    <Cell num={2}/>
 
                 </tr>
                 <tr>
-                    <Cell/>
-                    <Cell/>
-                    <Cell/>
+                    <Cell num={3}/>
+                    <Cell num={4}/>
+                    <Cell num={5}/>
 
                 </tr> 
                 <tr>
-                    <Cell/>
-                    <Cell/>
-                    <Cell/>
+                    <Cell num={6}/>
+                    <Cell num={7}/>
+                    <Cell num={8}/>
 
                 </tr>
             </body>
