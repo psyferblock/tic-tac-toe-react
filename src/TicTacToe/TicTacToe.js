d47e22a7d44de0e15a265b4343d935dee1d40640
psyfer <psyferblock.io@gmail.com>
2022-06-14 20:50:50 +0300

    winning logic

--- src/TicTacToe/TicTacToe.js
+++ src/TicTacToe/TicTacToe.js
@@ -3,42 +3,70 @@ import './TicTacToe.css';
 
 const TicTacToe = () => {
     const [turn,setTurn]=useState('x');
-    const[cells,setCells]=useState[Array(9).fill('')]
-    const handleClick=(num)=>{
-        let squares=[...cells]
-        if (turn=='x'){
-            setTurn('o')
-        }else{
-            setTurn('x')
+    const[cells,setCells]=useState(Array(9).fill(''))
+
+    const checkForWinner=(squares)=>{
+        let combos={
+            across:[
+                [0,1,2],
+                [3,4,5],
+                [6,7,8],
+            ],
+            diagonal:[
+                [0,4,8],
+                [2,4,6],
+            ],
+            down:[
+                [0,3,6],
+                [1,4,7],
+                [2,5,8]
+            ],
         }
+
     }
-    const Cell=()=>{
-        return <td onClick={()=>handleClick()}>-</td>
+
+    const handleClick=(num)=>{
+        if (cells[num]!==''){
+            alert('already clicked')
+            return;
+        }
+        let squares=[...cells]
+        if (turn==='x'){
+            squares[num]='x';
+            setTurn('o');
+        }else{
+            squares[num]='o';
+            setTurn('x');
+        }
+        setCells(squares); 
+        checkForWinner(squares)
+    }
+    const Cell=({num})=>{
+        return <td onClick={()=>handleClick(num)}>
+           { cells[num]}
+            </td>
     }
   return (
     <div className='container'>
         <table>
             Turn:{turn}
-            <body>
+            <tbody>
                 <tr>
                     <Cell num={0}/>
                     <Cell num={1}/>
                     <Cell num={2}/>
-
                 </tr>
                 <tr>
                     <Cell num={3}/>
                     <Cell num={4}/>
                     <Cell num={5}/>
-
                 </tr> 
                 <tr>
                     <Cell num={6}/>
                     <Cell num={7}/>
                     <Cell num={8}/>
-
                 </tr>
-            </body>
+            </tbody>
         </table>
     </div>
   )
